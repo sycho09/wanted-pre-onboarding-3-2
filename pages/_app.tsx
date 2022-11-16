e5f4e45568a97814b3cfb637d7b9fc5ab9dee7aa
sycho09 <wyswid@naver.com>
2022-11-17 08:53:57 +0900

    feat: basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react';
+import type { AppProps } from 'next/app';
+import { NextPage } from 'next';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'styles/GlobalStyle';
+import theme from 'styles/theme';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return getLayout(
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
---
+++ styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: 'rgb(5, 21, 38)',
+    secondary: '#0070f3',
+    grey: '#e7e7e7',
+  },
+  font: {},
+};
+
+export default theme;
